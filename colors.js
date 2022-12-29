1e2c68bee4b3881c312b437daf1eb83ecad187e6
Sanskar khandelwal <Sanskarkhandelwal336@gmail.com>
2022-12-29 21:28:49 +0530

    added a path for the specific file

--- colors.js
+++ colors.js
@@ -1,31 +1,45 @@
 const colors = [
   {
+    id: "1",
     color: "red",
     value: "#f00",
+    disc: "this is a unique color",
   },
   {
+    id: "2",
     color: "green",
     value: "#0f0",
+    disc: "this is a unique color",
   },
   {
+    id: "3",
     color: "blue",
     value: "#00f",
+    disc: "this is a unique color",
   },
   {
+    id: "4",
     color: "cyan",
     value: "#0ff",
+    disc: "this is a unique color",
   },
   {
+    id: "5",
     color: "magenta",
     value: "#f0f",
+    disc: "this is a unique color",
   },
   {
+    id: "6",
     color: "yellow",
     value: "#ff0",
+    disc: "this is a unique color",
   },
   {
+    id: "7",
     color: "black",
     value: "#000",
+    disc: "this is a unique color",
   },
 ];
 
--- day6.js
+++ day6.js
@@ -3,8 +3,27 @@ const { colors } = require("./colors");
 const express = require("express");
 const app = express();
 
+// app.get("/", (req, res) => {
+//   res.json(colors);
+// });
+
 app.get("/", (req, res) => {
-  res.json(colors);
+  res.send('<h1> Home page </h1> <a href="/api/colors">colors</a>');
+});
+
+app.get("/api/colors", (req, res) => {
+  const newColors = colors.map((id) => {
+    const { color, value } = id;
+    return { color, value };
+  });
+  res.json(newColors);
+});
+
+app.get("/api/colors/1", (req, res) => {
+  console.log(colors);
+  const singleColor = colors.find((single) => single.id == "1");
+  console.log(singleColor);
+  res.json(singleColor);
 });
 
 app.listen(5000, () => {
