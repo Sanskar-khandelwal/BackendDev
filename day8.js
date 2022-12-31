fae892a8fd9903c3ff02943d141385127281c7bc
Sanskar khandelwal <Sanskarkhandelwal336@gmail.com>
2022-12-31 14:02:38 +0530

    added the day8 in whic I started learning middleware, last commit was wrong

--- day8.js
+++ day8.js
@@ -4,23 +4,22 @@
 const express = require("express");
 const app = express();
 
-// req => middleware => res 
+// req => middleware => res
 // you have to always call next middleware or you have to send the respond and terminate the cycle
 const logger = (req, res, next) => {
-    const method = req.method;
-    const url = req.url;
-    const time = new Date().getFullYear();
-    console.log(method, url, time);
-    next(); 
-}
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  console.log(method, url, time);
+  next();
+};
 
-app.get('/', logger,  (req, res)=> {
-res.send("this is The home");
-
-})
-app.get('/about', (req, res)=> {
-res.send("this is The about");
-})
+app.get("/", logger, (req, res) => {
+  res.send("this is The home");
+});
+app.get("/about", (req, res) => {
+  res.send("this is The about");
+});
 
 app.listen(5000, () => {
   console.log("server is listening on port 5000...");
