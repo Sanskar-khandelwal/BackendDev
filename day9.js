14b7aa4b944a9215e5429c60b3591eec9d8971c6
Sanskar khandelwal <Sanskarkhandelwal336@gmail.com>
2023-01-04 12:57:24 +0530

    Learned how to collect the form data using express

--- day9.js
+++ day9.js
@@ -1,1 +1,22 @@
-// Learning about get, post, put, delete   
+// Learning about get, post, put, delete
+
+const express = require("express");
+const app = express();
+
+//middleware to get the static assets
+app.use(express.static("./methods-static"));
+
+// Middleware for parse form data, or collecting the form data
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/login", (req, res) => {
+  const { profession } = req.body;
+  if (profession) {
+    return res.send(`Hello, We know you are good at ${profession}`);
+  }
+  res.send("<h1>Please provide the proper credentioals </h1>");
+});
+
+app.listen(5000, () => {
+  console.log("listening on port number 5000");
+});
